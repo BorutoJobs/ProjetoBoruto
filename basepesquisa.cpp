107ecfcd495bfbe2507f1d659022df691a4dfc90
Diego <dcandrade19@users.noreply.github.com>
2017-06-19 20:26:26 -0300

    Base de codigo Pesquisa

---
+++ basepesquisa.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv)
+{
+	int i,s=232;
+	char nome[6],toere[10],endereco[10],cpf[30];
+	
+	strcpy(nome,"Pedro");
+	strcpy(endereco,"rua teste");
+	strcpy(cpf,"123.456.789-10");
+while(s!=0)
+{
+	printf("\n pesquisa nome: \n");
+	gets(toere);
+	
+     strlwr(nome);
+     strlwr(toere);
+     strlwr(e10 8
+	 bubbleSortt()
+     strlwr()
+     {
+     	coloeuqe tjens
+     }
+
+    printf("\n nome original: %s",nome);
+    printf("\n end original: %s",endereco);
+    printf("\n end original: %s",cpf);
+    
+if((strcmp(nome,toere)==0) || (strcmp(endereco,toere)==0) || (strcmp(cpf,toere)==0))
+{
+	printf("\n igual ");
+}
+else
+{
+		printf("\n diferente ");
+		
+}	
+}
+}
